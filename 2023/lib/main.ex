ecd5a6808ae5e63b3e50b9996dd4c1664950de38
Jonathan Lundholm <jon.lundholm@gmail.com>
2023-12-07 22:19:27 +0100

    feat(2023): print time

--- 2023/lib/main.ex
+++ 2023/lib/main.ex
@@ -1,14 +1,21 @@
 defmodule Advent.Main do
   alias Advent.Util.Input
 
-  def main(_args) do
+  def main([]) do
+    {time, result} = :timer.tc(fn -> run() end)
+
+    IO.puts("#{result}\n")
+    IO.puts("Ran in #{time / 1000} ms")
+  end
+
+  defp run() do
     days()
     |> Enum.map(&get_input/1)
     |> Enum.map(fn {input, mod} ->
       Task.async(mod, :run, [input])
     end)
     |> Task.await_many(30_000)
-    |> Enum.each(&IO.puts/1)
+    |> Enum.join("\n")
   end
 
   defp get_input({day, mod, :lines}) do
@@ -35,9 +42,8 @@ defmodule Advent.Main do
       {5, Advent.Day5, :lines_no_trim},
       {6, Advent.Day6, :lines},
       {7, Advent.Day7, :lines}
-
     ]
   end
 
-  defp input_file(day), do: "inputs/day#{day}.txt" 
+  defp input_file(day), do: "inputs/day#{day}.txt"
 end
